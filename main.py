2700d33623e2d56daaac4fa6a6002cb91370ebac
Théo Vicente <vtheo.ludon@gmail.com>
2022-04-28 16:36:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from time import sleep
 from classes.Labrynth import Labrynth
-from mazes.maze_2 import lab
+from mazes.maze_1 import lab
 
 if __name__ == '__main__':
     l = Labrynth(lab)
-    l.run(['right', 'right', 'down', 'right', 'down', 'down', 'right', 'down', 'right', 'down', 'right', 'down', 'right', 'down'])
+    l.run(['right', 'right', 'down', 'right', 'down', 'down', 'right', 'down', 'right', 'down', 'right', 'down', 'right', 'down', 'right', 'down', 'right', 'down', 'right', 'down', 'right', 'down', 'right', 'right'])
